7a719389e241972480af0d786da53b1f69d18de0
Andy Matuschak <andy@khanacademy.org>
2018-08-22 11:03:21 -0700

    Added redirect

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,8 @@ git clone --branch gh-pages https://github.com/Khan/free-response-report.git /tm
 mkdir -p /tmp/publish/open-ended
 rsync -rv --exclude=.git ./build/ /tmp/publish/open-ended
 
+echo "<html xmlns=\"http://www.w3.org/1999/xhtml\"><head><title>Early Product Development</title><meta http-equiv=\"refresh\" content=\"0;URL='/open-ended'\" /></head><body></body></html>" > /tmp/publish/index.html
+
 cd /tmp/publish
 
 git add -f .
